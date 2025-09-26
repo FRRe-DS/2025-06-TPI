a1982e457170fbbf7cadf3eb89cf75dffbf13154
GOYA <francomanuc@gmail.com>
2025-09-26 04:37:12 +0000

    All ShippingModels created - only error left

---
+++ src/ApiDePapas/Models/CancelShippingResponse.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnotations;
+
+namespace ApiDePapas.Models
+{
+    public class CancelShippingResponse
+    {
+        [Required]
+        public int shipping_id { get; set; }
+
+        [Required]
+        public ShippingStatus status { get; set; }
+
+        [Required]
+        public DateTime cancelled_at { get; set; }
+    }
+}
