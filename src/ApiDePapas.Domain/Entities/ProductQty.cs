7b7c0e510deb5620532f2838bf614fa17dd29ec9
GOYA <francomanuc@gmail.com>
2025-10-17 11:28:11 +0000

    Cambios iniciales para hacer merge con cosas de dev (carla y agustín)

--- src/ApiDePapas.Domain/Entities/ProductQty.cs
+++ src/ApiDePapas.Domain/Entities/ProductQty.cs
@@ -7,6 +7,8 @@ namespace ApiDePapas.Domain.Entities
 {
     // Modelo de SALIDA (Detalle): Representa un producto dentro de un envío ya creado.
     public record ProductQty(
+
+        // lo siguiente es id del producto, lo sacamos de stock y lo guardamos (relacionamos con el id del shipping)
         [property: JsonPropertyName("product_id")]
         [Required]
         int id,
