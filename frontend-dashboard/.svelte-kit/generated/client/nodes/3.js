8bec98bc6e12a0cec63b7e36b22890761586c23e
GOYA <francomanuc@gmail.com>
2025-10-30 15:48:38 +0000

    stop tracking .svelte

--- frontend-dashboard/.svelte-kit/generated/client/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/shipments/[id]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/shipments/[id]/+page.svelte";
