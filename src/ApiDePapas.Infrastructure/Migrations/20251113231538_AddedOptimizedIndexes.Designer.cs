ef193fb51210d58b0f17999f532587bb49e89b6d
GOYA <francomanuc@gmail.com>
2025-11-13 23:29:06 +0000

    Database Optimization test

--- src/ApiDePapas.Infrastructure/Migrations/20251113231538_AddedOptimizedIndexes.Designer.cs
+++ src/ApiDePapas.Infrastructure/Migrations/20251113231538_AddedOptimizedIndexes.Designer.cs
@@ -4,6 +4,7 @@ using ApiDePapas.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ApiDePapas.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251113231538_AddedOptimizedIndexes")]
+    partial class AddedOptimizedIndexes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
